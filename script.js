83b384632838b2cc9340575bbc7287e65537b09e
Arthur Loza <loza.uns@gmail.com>
2017-08-05 14:15:16 +0300

    HW16_Promise: task done

---
+++ script.js
@@ -0,0 +1,23 @@
+var deferred = $.Deferred();
+
+$(window)
+    .on('load', runPreloader)
+    .on('transitionend', startHiding);
+
+function runPreloader () {
+    $('.loadLine').css('width', 0);
+}
+
+function startHiding () {
+    deferred
+        .resolve()
+        .then(function() {
+            return $('article img').fadeOut(1000);
+        })
+        .then(function() {
+            return $('article p').fadeOut(1000);
+        })
+        .then(function() {
+            return $('article').fadeOut(1000);
+        })
+}
